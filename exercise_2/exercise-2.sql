835c10aa99117da1a1f59d04c1d89f73f9633f30
Zack Lyon <zack.j.lyon@gmail.com>
2022-01-31 13:03:59 -0800

    Add SQL queries and passing tests

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,7 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+
+SELECT first_name FROM customer
+INNER JOIN rental
+ON customer.customer_id = rental.customer_id
+WHERE customer.first_name = 'Patricia';
